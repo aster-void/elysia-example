8585b94c9462665593beb3a035b57b9d36da7393
aster <137767097+aster-void@users.noreply.github.com>
2025-01-23 22:23:29 +0900

    install zod and explain diff

--- client/main.ts
+++ client/main.ts
@@ -1,20 +1,27 @@
 import { edenFetch } from "@elysiajs/eden";
 import type { App } from "../server/main.ts";
+import { z } from "zod";
+
+const UserSchema = z.object({
+	name: z.string(),
+	thisPropertyDoesntExist: z.number(),
+});
+type User = z.infer<typeof UserSchema>;
 
 if (Bun.argv.length !== 3) {
 	console.error("usage: bun ./client/main.ts (your name)");
 	process.exit(1);
 }
-const fetch = edenFetch<App>("http://localhost:3000");
+const elysiaFetch = edenFetch<App>("http://localhost:3000");
 
 // 作成前
-const { data: users } = await fetch("/users", {
+const { data: users } = await elysiaFetch("/users", {
 	method: "GET",
 });
 console.log("before", users);
 
 // 作成
-const { data } = await fetch("/users", {
+const { data } = await elysiaFetch("/users", {
 	method: "POST",
 	body: {
 		name: Bun.argv[2], // 名前
@@ -23,13 +30,22 @@ const { data } = await fetch("/users", {
 console.log("new user:", data);
 
 // 作成後
-const { data: users2 } = await fetch("/users", {
+const { data: users2 } = await elysiaFetch("/users", {
 	method: "GET",
 });
 console.log("after", users2);
 
 // error
-// const this_error_at_comptime = await fetch("/users", {
-// 	method: "POST",
-// 	body: {},
-// });
+const { data: valid_user_type } = await elysiaFetch("/users", {
+	method: "GET",
+});
+valid_user_type?.thisPropertyDoesntExist;
+
+const resp = await fetch("/users", {
+	method: "GET",
+	// method: "POST",
+	// body: JSON.stringify({}),
+});
+// this error at runtime, because zod is a runtime library
+const dat = await resp.json();
+console.log(dat?.thisPropertyDoesntExist);
