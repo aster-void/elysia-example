c1c4d75250ab1c0d85f75893c660cc5cc940f2b8
aster <137767097+aster-void@users.noreply.github.com>
2025-01-23 23:45:21 +0900

    fix comment and code example because it was outdated

--- client/main.ts
+++ client/main.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 
 const UserSchema = z.object({
 	name: z.string(),
-	thisPropertyDoesntExist: z.number(),
+	thisPropertyDoesNotExist: z.number(),
 });
 type User = z.infer<typeof UserSchema>;
 
@@ -12,40 +12,54 @@ if (Bun.argv.length !== 3) {
 	console.error("usage: bun ./client/main.ts (your name)");
 	process.exit(1);
 }
+// this is usually called `fetch`,
+// but to contrast against web standard `fetch` API,
+// this is assigned to to `elysiaFetch`
 const elysiaFetch = edenFetch<App>("http://localhost:3000");
 
-// 作成前
-const { data: users } = await elysiaFetch("/users", {
-	method: "GET",
-});
-console.log("before", users);
+{
+	// 作成前
+	const { data: users } = await elysiaFetch("/users", {
+		method: "GET",
+	});
+	console.log("before", users);
+}
 
-// 作成
-const { data } = await elysiaFetch("/users", {
-	method: "POST",
-	body: {
-		name: Bun.argv[2], // 名前
-	},
-});
-console.log("new user:", data);
+{
+	// 作成
+	const { data } = await elysiaFetch("/users", {
+		method: "POST",
+		body: {
+			// OK. try removing this line.
+			name: Bun.argv[2], // 名前
+		},
+	});
+	console.log("new user:", data);
+}
 
-// 作成後
-const { data: users2 } = await elysiaFetch("/users", {
-	method: "GET",
-});
-console.log("after", users2);
+{
+	// 作成後
+	const { data: users } = await elysiaFetch("/users", {
+		method: "GET",
+	});
+	console.log("after", users);
+}
 
-// error
-const { data: valid_user_type } = await elysiaFetch("/users", {
-	method: "GET",
-});
-valid_user_type?.thisPropertyDoesntExist;
+{
+	// errors at comptime, without any kind of execution involved
+	const { data } = await elysiaFetch("/users", {
+		method: "GET",
+	});
+	data?.thisPropertyDoesNotExist; // Property 'thisPropertyDoesNotExist' does not exist on type ' { id: number, name: string }[]'
+}
 
-const resp = await fetch("/users", {
-	method: "GET",
-	// method: "POST",
-	// body: JSON.stringify({}),
-});
-// this error at runtime, because zod is a runtime library
-const dat = await resp.json();
-console.log(dat?.thisPropertyDoesntExist);
+{
+	// this error at runtime, because zod is a runtime library
+	const resp = await fetch("/users", {
+		method: "GET",
+		// method: "POST",
+		// body: JSON.stringify({}),
+	});
+	const data = UserSchema.parse(await resp.json());
+	console.log(data.thisPropertyDoesNotExist.toString()); // typescript says OK, but your browser / runtime won't.
+}
